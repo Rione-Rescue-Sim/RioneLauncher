ac704960e8f34f2caf890d064e34cd2b22a9f3bd
taka0628 <takatsun15@gmail.com>
2021-02-26 15:37:44 +0900

    同期

--- test.sh
+++ test.sh
@@ -4,15 +4,5 @@ LOCATION=$(cd; pwd)
 echo "$0"
 echo "$LOCATION"
 
-PATH_SCORE="/score.csv"
-PATH_GDRIVE="/gdrive/remote/"
-
-ROOT_PATH=$(cd; pwd)
-PATH_SCORE=${ROOT_PATH}${PATH_SCORE}
-PATH_GDRIVE=${ROOT_PATH}${PATH_GDRIVE}
-
-echo "PATH_SCORE: $PATH_SCORE"
-echo "PATH_GDRIVE: $PATH_GDRIVE"
-
-cd "${PATH_GDRIVE%/*}"
 pwd
+git checkout main
